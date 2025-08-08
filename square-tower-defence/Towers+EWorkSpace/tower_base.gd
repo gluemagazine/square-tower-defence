2cd1d5bc49f60ada73bf74cf6c6b9f87f9f24ab1
arrinrimmish <erinrimmish@gmail.com>
2025-08-08 10:10:36 -0600

    start of tower
    not much here

---
+++ square-tower-defence/Towers+EWorkSpace/tower_base.gd
@@ -0,0 +1,23 @@
+extends Node2D
+class_name TowerBase
+
+@export var options:Node2D
+
+func _on_click_pressed() -> void:
+	options.visible = true
+
+
+func frost_click() -> void:
+	pass # Replace with function body.
+
+
+func archer_clicked() -> void:
+	pass # Replace with function body.
+
+
+func bomb_clicked() -> void:
+	pass # Replace with function body.
+
+
+func summoner_clicked() -> void:
+	pass # Replace with function body.
