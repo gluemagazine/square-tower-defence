385821710fb9b16afa56f87805527c0c53d6646c
gluemagazine <126933672+gluemagazine@users.noreply.github.com>
2025-08-07 18:35:39 -0600

    set up navigation regions on the tilesets

--- square-tower-defence/enemies/enemy_template.gd
+++ square-tower-defence/enemies/enemy_template.gd
@@ -3,10 +3,19 @@ extends CharacterBody2D
 @export var panel : Panel
 @export var animation : AnimationPlayer
 @export var health : HealthComponent
+@export var target : Marker2D
+@export var nav : NavigationAgent2D
+@export var path_num : int = 1
 var shader : Shader = preload("uid://chudojcwfpmos").duplicate()
 var speed
 
 func _ready() -> void:
+	if target:
+		nav.target_position = target.global_position
+	else:
+		print("no target")
+	nav.set_navigation_layer_value(1,false)
+	nav.set_navigation_layer_value(pow(2,path_num-1),true)
 	speed = stats.speed
 	
 	health.max_health = stats.health
@@ -16,4 +25,6 @@ func _ready() -> void:
 	panel.material.set_shader_parameter("edges",stats.sides)
 	
 	animation.play(stats.animations["walk"])
-	
+
+func _physics_process(delta: float) -> void:
+	nav.get_next_path_position()
